0ddbaf4adfa7ef27a06b26ad7fa9bdb7dd4208cf
wipdev-tech <wipdev.tech@gmail.com>
2024-04-16 16:27:44 +0200

    Set up DB connection

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/wipdev-tech/musiqa
 
 go 1.22.2
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240416075003-747366ff79c4
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	dbToken := os.Getenv("DB_TOKEN")
+
+	connURL := fmt.Sprintf("%s?authToken=%s", dbURL, dbToken)
+	db, err := sql.Open("libsql", connURL)
+	if err != nil {
+		log.Fatal("failed to open SQL conn", err)
+	}
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal("failed to ping db", err)
+	}
 	fmt.Println("vim-go")
 }
